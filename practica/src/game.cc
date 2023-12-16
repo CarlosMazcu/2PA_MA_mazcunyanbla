fafa81f0f5a77556f16a769f2527e8a488f7a681
Calatayudbri <118178071+Calatayudbri@users.noreply.github.com>
2023-12-16 17:08:58 +0100

    fixed torchic animation and scale problems

--- practica/src/game.cc
+++ practica/src/game.cc
@@ -39,7 +39,7 @@ void initAnimationConfigManektrik()
   GameManager &GM = GameManager::Instance();
   GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {GM.windowSize.x, 250.0f}, {15.0f, 250.0f}, 2.5f, 
+              {GM.windowSize.x / 1.5f, 250.0f / 1.5f}, {15.0f / 1.5f, 250.0f / 1.5f}, 2.5f, 
               0.0f, 0.0f, 0.0f, 
               {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
               
@@ -54,7 +54,7 @@ void initAnimationConfigMainCharacter()
 {
   GameManager &GM = GameManager::Instance();
   GM.all_config_.ac_maincharacter_ = {
-      1, 0, 0, {600.0f*0.75f, 250.0f}, {580.0f * 0.75f, 250.0f}, 1.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+      1, 0, 0, {600.0f / 1.5f, 250.0f}, {580.0f / 1.5f, 280.0f}, 1.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
 
   for (int i = 0; i < 8; i++)
   {
@@ -69,7 +69,7 @@ void initAnimationConfigVolbeat()
   GameManager &GM = GameManager::Instance();
   GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {GM.windowSize.x * 0.75f, 225.0f},{GM.windowSize.x * 0.75f, 225.0f},2.5f,
+                {GM.windowSize.x / 1.5f, 225.0f},{GM.windowSize.x / 1.5f, 225.0f},2.5f,
                 0.0f,0.0f,0.0f,
                 {1.0f, 1.0f},{1.0f,1.0f}, 0.0f };
   for(int i = 0; i < 2; i++)
@@ -84,15 +84,11 @@ void initAnimationConfigTorchic()
 {
   GameManager &GM = GameManager::Instance();
   GM.all_config_.ac_torchic_ = {
-      1, 0, 0,
-      {GM.windowSize.x, 250.0f},
-      {600.0f, 250.0f},3.5f,
-      0.0f,0.0f,0.0f,
-      {1.0f, 1.0f},{1.0f, 1.0f},0.0f};
-      for (int i = 0; i < 5; i++)
-      {
-        GM.all_sprites.torchic[i].phase_ = 0;
-        GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_,&GM.all_sprites.torchic[i]);
+      1, 0, 0, {GM.windowSize.x / 1.5f, 250.0f}, {550.0f / 1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+  for (int i = 0; i < 5; i++)
+  {
+    GM.all_sprites.torchic[i].phase_ = 0;
+    GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_, &GM.all_sprites.torchic[i]);
       }
 }
 
@@ -105,7 +101,7 @@ void updateAnimationVolbeat()
     {
       GM.all_sprites.volbeat[i].phase_++;
 
-      if(GM.all_sprites.volbeat[i].phase_ > 6)
+      if(GM.all_sprites.volbeat[i].phase_ > 13)
       {
         GM.all_sprites.volbeat[i].phase_ = 0;
       }
@@ -115,9 +111,9 @@ void updateAnimationVolbeat()
       case 0:
         GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {GM.windowSize.x * 0.75f, 225.0f},{GM.windowSize.x * 0.75f, 225.0f},2.5f,
+                {GM.windowSize.x / 1.5f, 225.0f},{GM.windowSize.x / 1.5f, 225.0f},2.5f,
                 0.0f,0.0f,0.0f,
-                {1.0f, 1.0f},{1.0f,1.0f}, 0.0f };
+                {1.0f, 1.0f},{1.0f,1.0f}, 0.0f};
 
         GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
                                                                        &GM.all_sprites.volbeat[i]);
@@ -125,7 +121,7 @@ void updateAnimationVolbeat()
       case 1:
         GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {GM.windowSize.x * 0.75f, 225.0f},{130.0f * 1.5f, 225.0f},1.0f,
+                {GM.windowSize.x / 1.5f, 225.0f},{130.0f / 1.5f, 225.0f},1.0f,
                 0.0f,0.0f,0.0f,
                 {1.0f, 1.0f},{1.0f,1.0f}, 0.0f};
         
@@ -135,7 +131,7 @@ void updateAnimationVolbeat()
       case 2:
         GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {130.0f * 1.5f, 225.0f},{140.0f * 1.5f, 235.0f},0.5f,
+                {130.0f / 1.5f, 225.0f},{130.0f / 1.5f, 225.0f},0.5f,
                 0.0f,0.0f,0.0f,
                 {1.0f, 1.0f},{1.0f,1.0f}, 0.0f};
         
@@ -145,9 +141,9 @@ void updateAnimationVolbeat()
       case 3:
         GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {140.0f * 1.5f, 235.0f},{160.0f * 1.5f, 210.0f},1.0f,
+                {130.0f / 1.5f, 225.0f},{140.0f / 1.5f, 235.0f},1.5f,
                 0.0f,0.0f,0.0f,
-                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+                {1.0f, 1.0f},{1.0f,1.0f}, 0.0f};
           
         GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
                                                                        &GM.all_sprites.volbeat[i]);
@@ -155,9 +151,9 @@ void updateAnimationVolbeat()
       case 4:
         GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {160.0f * 1.5f, 210.0f},{160.0f * 1.5f, 210.0f},1.5f,
+                {140.0f / 1.5f, 235.0f},{140.0f / 1.5f, 235.0f},1.0f,
                 0.0f,0.0f,0.0f,
-                {1.0f, 1.0f},{1.5f,1.5f}, 1.5f};
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
           
         GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
                                                                        &GM.all_sprites.volbeat[i]);
@@ -165,7 +161,7 @@ void updateAnimationVolbeat()
       case 5:
         GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {160.0f * 2.25f, 210.0f},{400.0f * 2.25f, 240.0f},1.5f,
+                {140.0f / 1.5f, 235.0f},{160.0f / 1.5f, 210.0f},1.5f,
                 0.0f,0.0f,0.0f,
                 {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
           
@@ -175,9 +171,79 @@ void updateAnimationVolbeat()
       case 6:
         GM.all_config_.ac_volbeat_ = {
                 1, 0, 0,
-                {400.0f * 2.25f, 240.0f},{GM.windowSize.x * 2.25f, 240.0f},2.0f,
+                {160.0f / 1.5f, 210.0f},{160.0f / 1.5f, 210.0f},1.0f,
                 0.0f,0.0f,0.0f,
-                {1.0f, 1.0f},{0.5f,0.5f}, 1.0f};
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+          
+        GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
+                                                                       &GM.all_sprites.volbeat[i]);
+        break;
+      case 7:
+        GM.all_config_.ac_volbeat_ = {
+                1, 0, 0,
+                {160.0f / 1.5f, 210.0f},{150.0f / 1.5f, 240.0f},0.5f,
+                0.0f,0.0f,0.0f,
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+          
+        GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
+                                                                       &GM.all_sprites.volbeat[i]);
+        break;
+      case 8:
+        GM.all_config_.ac_volbeat_ = {
+                1, 0, 0,
+                {150.0f / 1.5f, 240.0f},{150.0f / 1.5f, 240.0f},0.5f,
+                0.0f,0.0f,0.0f,
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+          
+        GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
+                                                                       &GM.all_sprites.volbeat[i]);
+        break;
+      case 9:
+        GM.all_config_.ac_volbeat_ = {
+                1, 0, 0,
+                {150.0f / 1.5f, 240.0f},{70.0f / 1.5f, 240.0f},1.0f,
+                0.0f,0.0f,0.0f,
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+          
+        GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
+                                                                       &GM.all_sprites.volbeat[i]);
+        break;
+      case 10:
+        GM.all_config_.ac_volbeat_ = {
+                1, 0, 0,
+                {70.0f / 1.5f, 240.0f},{70.0f / 1.5f, 240.0f},0.3f,
+                0.0f,0.0f,0.0f,
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+          
+        GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
+                                                                       &GM.all_sprites.volbeat[i]);
+        break;
+      case 11:
+        GM.all_config_.ac_volbeat_ = {
+                1, 0, 0,
+                {70.0f / 1.5f, 240.0f},{170 / 1.5f, 240.0f},1.5f,
+                0.0f,0.0f,0.0f,
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+          
+        GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
+                                                                       &GM.all_sprites.volbeat[i]);
+        break;
+      case 12:
+        GM.all_config_.ac_volbeat_ = {
+                1, 0, 0,
+                {170 / 1.5f, 240.0f},{170 / 1.5f, 240.0f},0.5f,
+                0.0f,0.0f,0.0f,
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
+          
+        GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
+                                                                       &GM.all_sprites.volbeat[i]);
+        break;
+      case 13:
+        GM.all_config_.ac_volbeat_ = {
+                1, 0, 0,
+                {170 / 1.5f, 240.0f},{GM.windowSize.x / 1.5f, 240.0f},1.5f,
+                0.0f,0.0f,0.0f,
+                {1.0f, 1.0f},{1.0f,1.0f}, 1.0f};
           
         GM.all_sprites.volbeat[i].animation_ = new AnimationInstance(GM.all_config_.ac_volbeat_,
                                                                        &GM.all_sprites.volbeat[i]);
@@ -305,7 +371,7 @@ void updateAnimationManektrik()
       case 0:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {GM.windowSize.x, 250.0f}, {15.0f, 250.0f}, 2.5f, 
+              {GM.windowSize.x / 1.5f, 250.0f / 1.5f}, {15.0f / 1.5f, 250.0f / 1.5f}, 2.5f, 
               0.0f, 0.0f, 0.0f, 
               {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
               
@@ -315,9 +381,9 @@ void updateAnimationManektrik()
       case 1:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {15.0f, 250.0f}, {30.0f, 230.0f}, 0.5f, 
+              {15.0f / 1.5f, 250.0f / 1.5f}, {30.0f / 1.5f, 230.0f / 1.5f}, 0.5f, 
               0.0f, 0.0f, 0.0f, 
-              {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              {1.0f, 1.0f}, {1.0f, 1.0f}, 1.0f};
               
             GM.all_sprites.manektrik[i].animation_ = new AnimationInstance(GM.all_config_.ac_manektrik_,
                                                                        &GM.all_sprites.manektrik[i]);
@@ -325,9 +391,9 @@ void updateAnimationManektrik()
       case 2:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {30.0f, 230.0f}, {50.0f, 210.0f}, 0.5f, 
+              {30.0f / 1.5f, 230.0f / 1.5f}, {50.0f / 1.5f, 210.0f / 1.5f}, 0.5f, 
               0.0f, 0.0f, 0.0f, 
-              {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              {1.0f, 1.0f}, {1.0f, 1.0f}, 1.0f};
               
             GM.all_sprites.manektrik[i].animation_ = new AnimationInstance(GM.all_config_.ac_manektrik_,
                                                                        &GM.all_sprites.manektrik[i]);
@@ -335,7 +401,7 @@ void updateAnimationManektrik()
       case 3:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {50.0f, 210.0f}, {100.0f, 210.0f}, 0.5f, 
+              {50.0f / 1.5f, 210.0f / 1.5f}, {100.0f /1.5f, 210.0f / 1.5f}, 0.5f, 
               0.0f, 0.0f, 0.0f, 
               {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
               
@@ -345,7 +411,7 @@ void updateAnimationManektrik()
       case 4:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {100.0f, 210.0f}, {120.0f, 230.0f}, 0.5f, 
+              {100.0f / 1.5f, 210.0f/1.5f}, {120.0f/1.5f, 230.0f/1.5f}, 0.5f, 
               0.0f, 0.0f, 0.0f, 
               {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
               
@@ -355,7 +421,7 @@ void updateAnimationManektrik()
       case 5:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {120.0f, 230.0f}, {100.0f, 250.0f}, 0.5f, 
+              {120.0f/1.5f, 230.0f/1.5f}, {100.0f / 1.5f, 250.0f / 1.5f}, 0.5f, 
               0.0f, 0.0f, 0.0f, 
               {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
               
@@ -365,7 +431,7 @@ void updateAnimationManektrik()
       case 6:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {100.0f, 250.0f}, {50.0f, 250.0f}, 0.5f, 
+              {100.0f / 1.5f, 250.0f/ 1.5f}, {50.0f/ 1.5f, 250.0f/ 1.5f}, 0.5f, 
               0.0f, 0.0f, 0.0f, 
               {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
               
@@ -375,7 +441,7 @@ void updateAnimationManektrik()
       case 7:
             GM.all_config_.ac_manektrik_ = {
               1, 0, 0, 
-              {50.0f, 250.0f}, {GM.windowSize.x, 250.0f}, 3.5f, 
+              {50.0f/ 1.5f, 250.0f/ 1.5f}, {GM.windowSize.x/ 1.5f, 250.0f/ 1.5f}, 3.5f, 
               0.0f, 0.0f, 0.0f, 
               {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
               
@@ -406,42 +472,42 @@ void updateAnimationMainCharacter()
         {
         case 0:
           GM.all_config_.ac_maincharacter_ = {
-              1, 0, 0, {600.0f * 0.75f, 200.0f}, {580.0f * 0.75f, 200.0f}, 1.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              1, 0, 0, {600.0f  /1.5f, 200.0f}, {580.0f  /1.5f, 250.0f}, 1.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
 
           GM.all_sprites.maincharacter[i].animation_ = new AnimationInstance(GM.all_config_.ac_maincharacter_,
                                                                          &GM.all_sprites.maincharacter[i]);
           break;
         case 1:
           GM.all_config_.ac_maincharacter_ = {
-              1, 0, 0, {580.0f* 0.75f, 200.0f}, {580.0f* 0.75f, 250.0f}, 3.0f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              1, 0, 0, {580.0f /1.5f, 250.0f}, {580.0f /1.5f, 250.0f}, 3.0f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
 
           GM.all_sprites.maincharacter[i].animation_ = new AnimationInstance(GM.all_config_.ac_maincharacter_,
                                                                          &GM.all_sprites.maincharacter[i]);
           break;
         case 2:
           GM.all_config_.ac_maincharacter_ = {
-              1, 0, 0, {580.0f* 0.75f, 250.0f}, {50.0f* 0.75f, 200.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              1, 0, 0, {580.0f /1.5f, 250.0f}, {50.0f /1.5f, 200.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
 
           GM.all_sprites.maincharacter[i].animation_ = new AnimationInstance(GM.all_config_.ac_maincharacter_,
                                                                          &GM.all_sprites.maincharacter[i]);
           break;
         case 3:
           GM.all_config_.ac_maincharacter_ = {
-            1, 0, 0, {50.0f* 0.75f, 200.0f}, {510.0f* 0.75f, 200.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+            1, 0, 0, {50.0f /1.5f, 200.0f}, {510.0f /1.5f, 200.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
 
           GM.all_sprites.maincharacter[i].animation_ = new AnimationInstance(GM.all_config_.ac_maincharacter_,
                                                                          &GM.all_sprites.maincharacter[i]);
           break;
         case 4:
           GM.all_config_.ac_maincharacter_ = {
-              1, 0, 0, {510.0f* 0.75f, 200.0f}, {510.0f* 0.75f, 200.0f}, 2.3f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              1, 0, 0, {510.0f /1.5f, 200.0f}, {510.0f /1.5f, 200.0f}, 2.3f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
 
           GM.all_sprites.maincharacter[i].animation_ = new AnimationInstance(GM.all_config_.ac_maincharacter_,
                                                                          &GM.all_sprites.maincharacter[i]);
           break;
         case 5:
           GM.all_config_.ac_maincharacter_ = {
-              1, 0, 0, {510.0f* 0.75f, 200.0f}, {-120.0f* 0.75f, 200.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              1, 0, 0, {510.0f /1.5f, 200.0f}, {-120.0f /1.5f, 200.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
 
           GM.all_sprites.maincharacter[i].animation_ = new AnimationInstance(GM.all_config_.ac_maincharacter_,
                                                                          &GM.all_sprites.maincharacter[i]);
@@ -468,34 +534,53 @@ void updateAnimationTorchic()
       switch (GM.all_sprites.torchic[i].phase_)
       {
       case 0:
+        GM.torchic_fall = false;
+        GM.torchic_lying = false;
         GM.all_config_.ac_torchic_ = {
-            1, 0, 0, {GM.windowSize.x * 1.5f, 250.0f}, {600.0f * 1.5f, 250.0f}, 2.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+            1, 0, 0, {GM.windowSize.x /1.5f, 250.0f}, {550.0f /1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+        GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_,&GM.all_sprites.torchic[i]);
+       
         break;
       
       case 1:
+        GM.torchic_fall = false;
+        GM.torchic_lying = false;
         GM.all_config_.ac_torchic_ = {
-            1, 0, 0, {600.0f * 1.5f, 250.0f}, {620.0f * 1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
-            break;
+            1, 0, 0, {550.0f /1.5f, 250.0f}, {590.0f /1.5f, 250.0f}, 4.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+        GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_, &GM.all_sprites.torchic[i]);
+
+        break;
       case 2:
+        GM.torchic_fall = false;
+        GM.torchic_lying = false;
         GM.all_config_.ac_torchic_ = {
-            1, 0, 0, {620.0f * 1.5f, 250.0f}, {630.0f * 1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+            1, 0, 0, {590.0f /1.5f, 250.0f}, {590.0f /1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+        GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_, &GM.all_sprites.torchic[i]);
+
         break;
         case 3:
         GM.torchic_fall = true;
-          GM.all_config_.ac_torchic_ = {
-              1, 0, 0, {630.0f * 1.5f, 250.0f}, {630.0f * 1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+        GM.all_config_.ac_torchic_ = {
+            1, 0, 0, {590.0f /1.5f, 250.0f}, {GM.windowSize.x /1.5f, 250.0f}, 0.3f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
         GM.torchic_lying = true;
+        GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_, &GM.all_sprites.torchic[i]);
+
         break;
         case 4:
+          GM.torchic_fall = true;
+          GM.torchic_lying = true;
           GM.all_config_.ac_torchic_ = {
-              1, 0, 0, {630.0f * 1.5f, 250.0f}, {680.0f * 1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
-        break;
+              1, 0, 0, {GM.windowSize.x /1.5f , 250.0f}, {690.0f /1.5f, 250.0f}, 4.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+          GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_, &GM.all_sprites.torchic[i]);
+
+          break;
 
         case 5:
           GM.torchic_fall = false;
           GM.all_config_.ac_torchic_ = {
-              1, 0, 0, {680.0f * 1.5f, 250.0f}, {-50.0f * 1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
+              1, 0, 0, {690.0f /1.5f, 250.0f}, {-120.0f /1.5f, 250.0f}, 3.5f, 0.0f, 0.0f, 0.0f, {1.0f, 1.0f}, {1.0f, 1.0f}, 0.0f};
           GM.torchic_lying = false;
+          GM.all_sprites.torchic[i].animation_ = new AnimationInstance(GM.all_config_.ac_torchic_, &GM.all_sprites.torchic[i]);
           break;
       }
     }
